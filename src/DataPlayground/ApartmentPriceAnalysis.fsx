dcf3107d126cde581bb2c74bbf0618935317cbfa
Dominik Kotecki <dominikus1910@gmail.com>
2017-05-21 12:38:42 +0200

    htmlprovider

---
+++ src/DataPlayground/ApartmentPriceAnalysis.fsx
@@ -0,0 +1,20 @@
+// Before running any code, invoke Paket to get the dependencies.
+//
+// You can either build the project (Ctrl + Alt + B in VS) or run
+// '.paket/paket.bootstrap.exe' and then '.paket/paket.exe install'
+// (if you are on a Mac or Linux, run the 'exe' files using 'mono')
+//
+// Once you have packages, use Alt+Enter (in VS) or Ctrl+Enter to
+// run the following in F# Interactive. You can ignore the project
+// (running it doesn't do anything, it just contains this script)
+#load "..\..\packages/FsLab/FsLab.fsx"
+
+open Deedle
+open FSharp.Data
+open XPlot.GoogleCharts
+open XPlot.GoogleCharts.Deedle
+
+type Offers = HtmlProvider<"https://dom.gratka.pl/mieszkania-do-wynajecia/lista/,,1,40,1,1,co,li,s,mo,lpo.html">
+
+
+
