c4e9a272b0a6302f1feb7edcfe3dcdc6ebc41f69
Dominik Kotecki <Dominik.Kotecki@rossmann.com.pl>
2017-07-17 11:35:11 +0200

    Pie chart sentiment

--- src/DataPlayground/SentimentFS.fsx
+++ src/DataPlayground/SentimentFS.fsx
@@ -17,5 +17,27 @@ open XPlot.GoogleCharts.Deedle
 let apiUrl = "http://localhost:5000/api/analysis/"
 type SentimentAnalysisResult = JsonProvider<"http://localhost:5000/api/analysis/java">
 
-let sentimentForFsharp = SentimentAnalysisResult.Load(apiUrl + "fsharp")
+let sentimentForFsharp = SentimentAnalysisResult.Load(apiUrl + "java")
 
+let getSentimentName(sentiment: int) =
+    match sentiment with
+    | -2 -> "Bardzo Negatywny"
+    | -1 -> "Negatywny"
+    | 0 -> "Neutralny"
+    | 1 -> "Pozytywny"
+    | 2 -> "Bardzo Pozytywny"
+    | _ -> "Neutralny"
+
+let sentimentChart = sentimentForFsharp.SentimentByQuantity |> Array.map(fun x -> (getSentimentName(x.Key), x.Value)) |> Chart.Pie |> Chart.WithTitle "Sentiment By Quantity (houseofcards)" |> Chart.WithLegend true
+
+
+let wordCloud =
+    Options(
+        title = "Kewords",
+        hAxis = Axis(title = "0y"),
+        vAxis = Axis(title = "Fertility Rate"),
+        bubble = Bubble(textStyle = TextStyle(fontSize = 11))
+    )
+
+//let options = Options(showTip = true)
+//let locations = sentimentForFsharp.Localizations |> Array.map(fun x -> (x.Latitude.ToString(), x.Longitude.ToString())) |> Chart.Map |> Chart.WithOptions options |> Chart.WithHeight 420
