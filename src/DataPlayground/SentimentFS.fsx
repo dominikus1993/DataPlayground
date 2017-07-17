b6488c4e872378903e96ee67c34afad4db4280b9
Dominik Kotecki <Dominik.Kotecki@rossmann.com.pl>
2017-07-17 09:29:00 +0200

    add SentimentFS DataPlayground

---
+++ src/DataPlayground/SentimentFS.fsx
@@ -0,0 +1,21 @@
+// Before running any code, invoke Paket to get the dependencies.
+//
+// You can either build the project (Ctrl + Alt + B in VS) or run
+// '.paket/paket.bootstrap.exe' and then '.paket/paket.exe install'
+// (if you are on a Mac or Linux, run the 'exe' files using 'mono')
+//
+// Once you have packages, use Alt+Enter (in VS) or Ctrl+Enter to
+// run the following in F# Interactive. You can ignore the project
+// (running it doesn't do anything, it just contains this script)
+#load "..\..\packages/FsLab/FsLab.fsx"
+
+open Deedle
+open FSharp.Data
+open XPlot.GoogleCharts
+open XPlot.GoogleCharts.Deedle
+
+let apiUrl = "http://localhost:5000/api/analysis/"
+type SentimentAnalysisResult = JsonProvider<"http://localhost:5000/api/analysis/java">
+
+let sentimentForFsharp = SentimentAnalysisResult.Load(apiUrl + "fsharp")
+
